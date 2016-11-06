2ef64c3af559c7e1bc307c64183e9608f52a0cbe
Joe Dai <joe.dai@orionhealth.com>
2016-11-06 15:49:43 +1300

    added a bunch of stuff

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class RatingControl: UIView {
 
+    // MARK: Properties
+    
+    var rating = 0{
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
+    
+    // MARK: Initialization
+    override func layoutSubviews() {
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.setImage(emptyStarImage, for: .normal)
+            button.setImage(filledStarImage, for: .selected)
+            button.setImage(filledStarImage, for: [.highlighted, .selected])
+            button.adjustsImageWhenHighlighted = false
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        get {
+            let buttonSize = Int(frame.size.height)
+            let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+            return CGSize(width: width, height: buttonSize)
+        }
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -18,4 +70,17 @@ class RatingControl: UIView {
     }
     */
 
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        rating = ratingButtons.index(of: button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
 }
